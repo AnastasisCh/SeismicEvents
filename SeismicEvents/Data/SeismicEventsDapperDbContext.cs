4b7a0867467cbb8440b656cb4dd3845eca891c40
anc <anastasishatzis@gmail.com>
2025-12-26 15:32:52 +0200

    Added dapper as orm for comparing performances

---
+++ SeismicEvents/Data/SeismicEventsDapperDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace SeismicEventsFireEvents.Data
+{
+    public class SeismicEventsDapperDbContext
+    {
+        private readonly IConfiguration _configuration;
+        public SeismicEventsDapperDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection CreateConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("SQLLiteConnection");
+            return new Microsoft.Data.Sqlite.SqliteConnection(connectionString);
+        }
+    }
+}
