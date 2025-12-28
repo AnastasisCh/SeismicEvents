f259a91c4781e7b3a202c06405daaf175d59819b
anc <anastasishatzis@gmail.com>
2025-12-28 23:18:11 +0200

    Added dapper depth endpoints for comparison with ef.

--- SeismicEvents/Controllers/FindSeismicEventsController.cs
+++ SeismicEvents/Controllers/FindSeismicEventsController.cs
@@ -20,7 +20,7 @@ namespace SeismicEventsFireEvents.Controllers
             _dapperDbContext = dapperDbContext;
         }
         //~100ms 6.7mb
-        [HttpGet("FindSeismicEventsEF/{flynnRegion}")]
+        [HttpGet("Compressed/FindSeismicEventsEF/{flynnRegion}")]
         public async Task<IActionResult> FindSeismicEventsEF(string flynnRegion)
         {
             IEnumerable<byte[]> compressedSeismicPropertyChunks= _dbContext.SeismicCompressed
@@ -29,26 +29,42 @@ namespace SeismicEventsFireEvents.Controllers
             IEnumerable<DTOs.SeismicProperties> decompressedSeismicProperties = SeismicEventsUtils.DecompressSeismicChunks(compressedSeismicPropertyChunks);
             return Ok(decompressedSeismicProperties);
         }
-        //~70ms 6.7mb
-        [HttpGet("FindSeismicEventsDapper/{flynnRegion}")]
+        //~100ms 6.7mb
+        [HttpGet("Compressed/FindSeismicEventsDapper/{flynnRegion}")]
         public async Task<IActionResult> FindSeismicEventsDapper(string flynnRegion)
         {
             IEnumerable<byte[]> compressedSeismicPropertiesDapper = Enumerable.Empty<byte[]>();
             using(var connection=_dapperDbContext.CreateConnection())
             {
-                compressedSeismicPropertiesDapper = await connection.QueryAsync<byte[]>("SELECT CompressedEventProperties FROM SeismicCompressed WHERE FlynnRegion=@FlynnRegion",new {FlynnRegion=flynnRegion});
+                compressedSeismicPropertiesDapper = await connection.QueryAsync<byte[]>(@"SELECT CompressedEventProperties 
+                                                                                            FROM SeismicCompressed 
+                                                                                            WHERE FlynnRegion=@FlynnRegion",new {FlynnRegion=flynnRegion});
             }
             IEnumerable<SeismicProperties> decompressedSeismicPropertiesDapper = SeismicEventsUtils.DecompressSeismicChunks(compressedSeismicPropertiesDapper);
             return Ok(decompressedSeismicPropertiesDapper);
         }
-        [HttpGet("Depth")]
-        public async Task<IActionResult> FindSeismicEvents([FromQuery] double minDepth, [FromQuery] double maxDepth)
+        [HttpGet("Compressed/FindSeismicDepthEF")]
+        public async Task<IActionResult> FindSeismicEventsEF([FromQuery] double minDepth = 0, [FromQuery] double maxDepth= 0)
         {
             IEnumerable<byte[]> compressedSeismicPropertyChunks = _dbContext.SeismicCompressed
                 .Where(se => se.MinDepth<=maxDepth && se.MaxDepth>=minDepth)
                 .Select(row => row.CompressedEventProperties).AsEnumerable();
             IEnumerable<DTOs.SeismicProperties> decompressedSeismicProperties = SeismicEventsUtils.DecompressSeismicChunks(compressedSeismicPropertyChunks).Where(se=>se.Depth>=minDepth && se.Depth<=maxDepth);
             return Ok(decompressedSeismicProperties);
+        }
+        [HttpGet("Compressed/FindSeismicDepthDapper")]
+        public async Task<IActionResult> FindSeismicEventsDapper([FromQuery] double minDepth=0, [FromQuery] double maxDepth=0)
+        {
+            IEnumerable<byte[]> comporessedProperties = Enumerable.Empty<byte[]>();
+            using (var connection=_dapperDbContext.CreateConnection())
+            {
+                comporessedProperties = await connection.QueryAsync<byte[]>(@"SELECT CompressedEventProperties 
+                                                                            FROM SeismicCompressed 
+                                                                            WHERE MinimumDepth<=@MaxDepth AND MaximumDepth>=@MinDepth", new { MaxDepth = maxDepth, MinDepth = minDepth });
+                
+            }
+            IEnumerable<SeismicProperties> decompressedSeismicPropertiesDapper = SeismicEventsUtils.DecompressSeismicChunks(comporessedProperties).Where(se => se.Depth >= minDepth && se.Depth <= maxDepth);
+            return Ok(decompressedSeismicPropertiesDapper);
         }
 
     }
