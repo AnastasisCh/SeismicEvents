0bc3e020f586215468c073d4dbcf97346543aacf
anc <anastasishatzis@gmail.com>
2025-12-29 13:05:22 +0200

    resolved

--- SeismicEvents/Program.cs
+++ SeismicEvents/Program.cs
@@ -12,8 +12,6 @@ namespace SeismicEventsFireEvents
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             //builder.Services.AddHostedService<PollingNASAFirms>();
             // builder.Services.AddHostedService<SeedData>();
             builder.Services.AddHostedService<SeismicPortalClientWS>();
@@ -25,18 +23,9 @@ namespace SeismicEventsFireEvents
             builder.Services.AddScoped<SeismicEventsDapperDbContext>();
 
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
 
             app.UseHttpsRedirection();
 
